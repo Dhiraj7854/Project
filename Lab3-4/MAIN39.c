400e3de35c93a0778486ae141a2a7efeb9e8f754
Dhiraj7854 <dhirajdubey2022@outlook.com>
2024-09-12 13:58:25 +0530

    Add files via upload

--- Lab3-4/MAIN39.c
+++ Lab3-4/MAIN39.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
     int n;
-    printf("Enter the numbers: ");
+    printf("Enter the number of elements: ");
     scanf("%d",&n);
 
-    
+    for(int i = 1;i <= n-1;i++)
+    { printf("%d/%d! + ",i,i);}
+
+    printf("%d/%d!",n,n);
 }
